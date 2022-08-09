248d9d334a308fe2140e345a77a0f9f013084086
Xemn <spysschaert.pro@gmail.com>
2022-08-09 16:13:11 +0200

    Firstly : Change the logic of my express middleware signup in order to create a unique admin user; Secondly : Create my express middleware who will allow a user to be connected

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -10,20 +10,95 @@ dotenv.config();
 
 // Création de notre middleware express concernant la création d'un utilisateur : 
 exports.signup = (req, res, next) => {
-    // On commence en premier lieu à haser notre mot de passe : 
-    bcrypt
-        .hash(req.body.password, 10)
-        .then ((hash) => {
-            /* Après l'obtention on peut créer et sauvegarder notre utilisateur
-            en base de données : */
-            const user = new User ({
-                email : req.body.email,
-                password : hash,
-            });
-            user
-                .save()
-                .then(() => res.status(201).json({message : "Utilisateur bien enregistré ! "}))
-                .catch((error) => res.status(400).json({error}))
+    // Nous allons d'abord voir si notre base de données est vide : 
+    User.findAll()
+    .then((result) => {
+        // Si elle l'est notre premier utilisateur sera admin : 
+        if (result.length == 0)
+        {
+            // On commence en premier lieu à haser notre mot de passe : 
+            bcrypt
+                .hash(req.body.password, 10)
+                .then ((hash) => {
+                    /* Après l'obtention on peut créer et sauvegarder notre utilisateur
+                    en base de données : */
+                    const user = new User ({
+                    email : req.body.email,
+                    password : hash,
+                    isAdmin : true 
+                    });
+                    user
+                        .save()
+                        .then(() => res.status(201).json({message : "Utilisateur bien enregistré ! "}))
+                        .catch((error) => res.status(400).json({error}))
+            })
+            .catch((error) => res.status(500).json({error}))
+        }
+        else 
+        {
+            // On commence en premier lieu à haser notre mot de passe : 
+            bcrypt
+                .hash(req.body.password, 10)
+                .then ((hash) => {
+                    /* Après l'obtention on peut créer et sauvegarder notre utilisateur
+                    en base de données : */
+                    const user = new User ({
+                    email : req.body.email,
+                    password : hash,
+                    isAdmin : false
+                    });
+                    user
+                        .save()
+                        .then(() => res.status(201).json({message : "Utilisateur bien enregistré ! "}))
+                        .catch((error) => res.status(400).json({error}))
+            })
+            .catch((error) => res.status(500).json({error}))
+        }
+    })
+    .catch((error) => res.status(500).json({error}))
+};
+
+// Création de notre middleware express concernant la connexion d'un utilisateur : 
+exports.login = (req, res, next) => {
+    /* On vérifie dans un premier temps que l'adresse mail entrée est présente 
+    dans notre base de données : */
+    User.findOne({email : req.body.email })
+        .then((user) => {
+            // Si nous ne trouvons pas d'adresse mail correspondante dans notre base : 
+            if (!user) {
+                return res.status(401).json({error : "Utilsateur introuvable !"});
+            }
+            // Si il existe : 
+            // Nous comparons le hash des deux mots de passe :
+            bcrypt
+            .compare(req.body.password, user.password)
+            .then((valid) => {
+                // Si les deux hash ne correspondent pas : 
+                if (!valid){
+                    return res.status(401).json({error : "Mot de passe incorrect !"});
+                }
+                // Si les deux hash correspondent : 
+                res.status(200).json({
+                    // On crée un objet contentant l'userID et un token : 
+                    userId : user._id,
+                    token : jwt.sign(
+                        // Payload : 
+                        { userId : user._id,
+                         isAdmin : user.isAdmin},
+                    
+                    // Chaîne secrète de développement temporaire (salt) :
+                    process.env.TOKEN_KEYPHRASE,
+                    
+                    // Son délai d'expiration :
+                    {
+                        expiresIn : "24h",
+                    }
+
+
+                    ),
+                });
+            })
+            .catch((error) => res.status(500).json({error}));
         })
-        .catch((error) => res.status(500).json({error}))
+        .catch((error) => res.status(500).json({error}));
 };
