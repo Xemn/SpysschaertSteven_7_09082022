6ad725372076ad3af8b2d5fee643c168c433c302
Xemn <spysschaert.pro@gmail.com>
2022-08-09 13:32:08 +0200

    Import of our moongose module; Connexion to our Mongo Database etablish; Creation of ou express application and finally export of our application

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -3,3 +3,21 @@ const express = require("express");
 //Import de notre module dotenv : 
 const dotenv = require("dotenv");
 dotenv.config();
+// Import de notre module mongoose : 
+const moongoose = require("mongoose");
+
+// Connexion de notre application à notre base de données mongoDB Atlas : 
+moongoose
+    .connect(process.env.HTTPS_PROXY, {
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+    })
+    .then (() => console.log("Connexion à MongoDB réussié"))
+    .catch(() => console.log("Connexion à MongoDB échouée"))
+
+// Création de notre application express : 
+const app = express();
+
+// Export de notre application : 
+module.exports = app;
+
