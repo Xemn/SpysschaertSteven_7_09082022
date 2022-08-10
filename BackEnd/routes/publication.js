02ae0ca14bd0155073654671d7ac939a793a2dc8
Xemn <spysschaert.pro@gmail.com>
2022-08-10 16:20:59 +0200

    Adding the put route who will use express middleware updateOneSauce in our controller publication.js to allow us to modify a publication in our database

--- BackEnd/routes/publication.js
+++ BackEnd/routes/publication.js
@@ -11,8 +11,10 @@ const auth = require("../middleware/auth");
 router.post("/", auth, publiCtrl.createPublication);
 // Route GET (Endpoint) pour la récupération de toutes les publications :
 router.get("/", auth, publiCtrl.getAllPublications);
-// Route Get (Endpoint) pour la récupération d'une publication précise : 
+// Route GET (Endpoint) pour la récupération d'une publication précise : 
 router.get("/:id", auth, publiCtrl.getOnePublication);
+// Route PUT (Endpoint) pour la modification d'une publication précise :
+router.put("/:id", auth, publiCtrl.updateOnePublication);
 
 // Export de nos routes : 
 module.exports = router;
