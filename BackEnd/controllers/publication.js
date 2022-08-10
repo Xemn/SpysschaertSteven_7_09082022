891f9e64eaa3073f9283365d2ecd9fcb4ea852ef
Xemn <spysschaert.pro@gmail.com>
2022-08-10 13:26:37 +0200

    Adding the express middleware who allow us to get all the publications sorting from newest to oldest

--- BackEnd/controllers/publication.js
+++ BackEnd/controllers/publication.js
@@ -1,7 +1,7 @@
 // Import de notre modèle Publication : 
 const Publication = require('../models/Publication');
 
-// Logique métier pour la création d'une sauce : 
+// Logique métier pour la création d'une publication : 
 exports.createPublication = (req, res, next) => {
     const publication = new Publication({
         ...req.body
@@ -11,4 +11,11 @@ exports.createPublication = (req, res, next) => {
         .then(() => res.status(201).json({message : "Publication enregistrée ! "}))
         .catch((error) => res.status(400).json({error}))
 
+};
+// Logique métier pour la récupération de toute les publications : ``
+exports.getAllPublications = (req, res, next) => {
+    Publication.find()
+    .sort({createdAt : -1})
+    .then((publications) => res.status(200).json(publications))
+    .catch((error) => res.status(400).json({error}))
 };
