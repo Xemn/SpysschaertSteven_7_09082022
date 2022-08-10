354903e2bd60a3f8b32c9f2cb2bdff4552ed771a
Xemn <spysschaert.pro@gmail.com>
2022-08-10 12:59:34 +0200

    Fixe an error in my User.js model : about the type String of likes and dislikes change into Number; createdAt field added in my model too

--- BackEnd/models/Publication.js
+++ BackEnd/models/Publication.js
@@ -6,11 +6,13 @@ const publicationSchema = mongoose.Schema({
     userId : { type: String, required: true },
     title : { type : String, required: true },
     message: { type: String, required: true },
-    likes : { type: String, default : 0 },
-    dislikes : { type: String, default : 0 },
+    createdAt : { type : Date, default: Date.now},
+    likes : { type: Number, default : 0 },
+    dislikes : { type: Number, default : 0 },
     usersLiked : { type: [String], default : [] },
     usersDiliked : { type: [String], default : []}
-});
+    }
+);
 
 // Export de notre Schéma publication en tant que modèle : 
 module.exports = mongoose.model("Publication", publicationSchema);
