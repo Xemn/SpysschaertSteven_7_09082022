a5c389d24503f6a8d2085ab35d191570a926fe99
Xemn <spysschaert.pro@gmail.com>
2022-09-12 14:44:40 +0200

    Import our multer middlware and use it on the post and put publication's routes

--- BackEnd/routes/publication.js
+++ BackEnd/routes/publication.js
@@ -6,15 +6,17 @@ const router = express.Router();
 const publiCtrl = require("../controllers/publication");
 // Import de notre middlware d'authentification :
 const auth = require("../middleware/auth");
+// Import de notre middlware multer : 
+const multer = require("../middleware/multer-config");
 
 // Route POST (Endpoint) pour la création d'une publication : 
-router.post("/", auth, publiCtrl.createPublication);
+router.post("/", auth, multer, publiCtrl.createPublication);
 // Route GET (Endpoint) pour la récupération de toutes les publications :
 router.get("/", auth, publiCtrl.getAllPublications);
 // Route GET (Endpoint) pour la récupération d'une publication précise : 
 router.get("/:id", auth, publiCtrl.getOnePublication);
 // Route PUT (Endpoint) pour la modification d'une publication précise :
-router.put("/:id", auth, publiCtrl.updateOnePublication);
+router.put("/:id", auth, multer, publiCtrl.updateOnePublication);
 // Route DELETE (Endpoint) pour la supression d'une publication précise : 
 router.delete("/:id", auth, publiCtrl.deletePublication);
 
