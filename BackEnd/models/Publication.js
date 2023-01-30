19f36c90d751c4a1cedf5f77ceb3f6af4ac1793f
Xemn <spysschaert.pro@gmail.com>
2023-01-30 17:52:24 +0100

    Remove the field title, remove the requiered option of imageUrl, and correct an error of miss spelling about usersDisliked

--- BackEnd/models/Publication.js
+++ BackEnd/models/Publication.js
@@ -4,14 +4,13 @@ const mongoose = require('mongoose');
 // Crétion de notre Schéma Publication : 
 const publicationSchema = mongoose.Schema({
     userId : { type: String, required: true },
-    title : { type : String, required: true },
     message: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    imageUrl: { type: String, default: ""},
     createdAt : { type : Date, default: Date.now},
     likes : { type: Number, default : 0 },
     dislikes : { type: Number, default : 0 },
     usersLiked : { type: [String], default : [] },
-    usersDiliked : { type: [String], default : []}
+    usersDisliked : { type: [String], default : []}
     }
 );
 
