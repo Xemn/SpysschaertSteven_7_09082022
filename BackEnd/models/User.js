a54e68565512f8b774fb74f75e68401b8d7d2fb0
Xemn <spysschaert.pro@gmail.com>
2023-01-30 17:53:07 +0100

    Adding a field username in my User model

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -6,6 +6,7 @@ const uniqueValidator = require("mongoose-unique-validator");
 // Crétation de notre schéma User : 
 const userSchema = mongoose.Schema({
     email : {type : String, required : true, unique : true },
+    username : {type : String, required: true}, 
     password : {type: String, required : true},
     isAdmin : {type : Boolean, default : false}
 });
