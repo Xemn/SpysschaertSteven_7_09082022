c1cbb59da8ff9fc4da83671ffd193813d66771f7
Xemn <spysschaert.pro@gmail.com>
2023-01-30 17:47:58 +0100

    Adding CORS in my app.js file

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -28,6 +28,21 @@ const app = express();
 devons les transformer en objet JSON :  */
 app.use(express.json());
 
+/* Applications d'entête afin d'aplliquer nos propres règles de 
+sécurité CORS : */
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", '*');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 // Indique à notre application Express de se servir du dossier static images :
 app.use("/images", express.static(path.join(__dirname, "images")));
 
